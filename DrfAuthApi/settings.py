016a877db540da286940535405c4206962bceded
Ali <Alihunter4640@gmail.com>
2024-06-20 18:36:16 +0330

    Django Custom User Model
    Custom User Made , By using two BaseUserManager and AbstractBaseUser classes , but i had a problem and issue for migrating that was about my first migration 0001.initial so i hash the admin url and app from settings.py and again run those two migration commands and at the end i unhashed the hashed items

--- DrfAuthApi/settings.py
+++ DrfAuthApi/settings.py
@@ -205,3 +205,7 @@ SIMPLE_JWT = {
     "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer", }
+
+
+
+AUTH_USER_MODEL = "accounts.User"
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,79 @@
+from tkinter import N
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email,name,remember_me ,password=None,password2=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name = name ,
+            remember_me = remember_me 
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, remember_me , password=None, password2=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            name = name ,
+            remember_me = remember_me
+
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+
+
+
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    name = models.CharField(max_length=255)
+    remember_me = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name","remember_me"]
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
