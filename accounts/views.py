a3d6d2377c779b67c8f0d893a7424d27c57bc048
Ali <Alihunter4640@gmail.com>
2024-06-21 18:23:44 +0330

    implement JWT
    Json Web Token system Set Up , the configure and it's function inserted in views

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,18 @@ from .serializers import UserRegisterSerializer, UserLoginSerializer
 from rest_framework import status
 from django.contrib.auth import authenticate
 from .renderers import UserRenderer 
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+# Json Web Token For Each User. 
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
 
 # Sign Up Class View 
 class UserCreation(APIView):
@@ -12,8 +24,9 @@ class UserCreation(APIView):
     def post(self, request, format=None):
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response({"msg":"You registered successfuly!"},status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            token = get_tokens_for_user(user)
+            return Response({"token":token ,"msg":"You registered successfuly!"},status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
@@ -28,7 +41,8 @@ class LoginView(APIView):
             password = serializer.data.get('password')
             user = authenticate(email=email,password=password)
             if user is not None:
-                return Response({"msg" : "Login Successful"} , status=status.HTTP_200_OK)
+                token = get_tokens_for_user(user)
+                return Response({"token":token,"msg" : "Login Successful"} , status=status.HTTP_200_OK)
             else:
                 return Response({"errors":"Login Faild , Email or Password is not valid !"} , status=status.HTTP_404_NOT_FOUND)
             
